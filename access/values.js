b38a21e0b0fd5b24b8b56ae48c7edbfa1d8d64ea
ko1oma <126311516+ko1oma@users.noreply.github.com>
2023-02-25 08:48:32 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "27.01.2001"; // Дата рождения
+var fullname = "Коломиец Максим Владимирович" // ФИО
+var name = "Максим" // Имя
+
+// Паспорт
+var passport_id = "228133711" // Номер паспорта
+
+// КПП
+var kpp_id = "9999999999" // ИНН
+
+// Права
+var rights_valid_until = "10.10.2065" // Права "Дійсні до"
+var rights_categories = "A, А1, В, В1" // Права "Категорії"
+var rights_tsc = "ТСЦ 3636" // Права "Видав
+var rights_id = "КАКАШКА968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "11.11.2011" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
